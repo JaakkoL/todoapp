e10c07bc2dd876ab0aa353ceea54120928721ee4
JaakkoL <jaakko.o.laurila@gmail.com>
2013-10-01 23:05:23 +0300

    updated documentation

--- model/users.js
+++ model/users.js
@@ -59,7 +59,6 @@ function UsersDAO(connection) {
       return error;
     }
 
-
     var query = 'SELECT * FROM user WHERE email = ' + connection.escape(email) + ' LIMIT 1';
     connection.query(query, function(err, user) {
       validateUser(err, user);
@@ -70,7 +69,6 @@ function UsersDAO(connection) {
   this.usernameExists = function(username, callback) {
 
     var query = 'SELECT * FROM user WHERE email = ' + connection.escape(username);
-
     connection.query(query, function(err, results) {
       var exists = (results.length === 0) ? false : true;
       callback(err, exists);
